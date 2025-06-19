4a6640e109da551f5732b55dd587cd00075c8021
Prouk <valentin.tahon2@gmail.com>
2025-06-19 22:38:03 +0200

    update of version.json

--- scripts/updateVersion.sh
+++ scripts/updateVersion.sh
@@ -1,7 +1,21 @@
+echo "Getting last tag from temp"
+echo "[" > version.json
+   
 curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GH_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/zen-browser-auto/www-temp/tags \
-  | jq -r '.[0].name' > resp.json
-  cat resp.json
+  | jq -r '.[0]' > version.json 
+
+echo "]" > version.json
+
+VERSION_CONTENT= cat version.json | base64
+
+curl -L \
+  -X PUT \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GH_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/Prouk/zen-browser-flake/contents/version.json \
+  -d '{"message":"version.js update","branch":"main","sha","committer":{"name":"Prouk (action)","email":"valentin.tahon2@gmail.com"},"content":'"${VERSION_CONTENT"'}}'
