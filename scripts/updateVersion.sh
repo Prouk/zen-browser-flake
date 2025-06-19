f984d681a40cb688b1737e464fd0dd6702f31abc
Prouk <valentin.tahon2@gmail.com>
2025-06-19 20:13:31 +0200

    get tags from temp zen

--- scripts/updateVersion.sh
+++ scripts/updateVersion.sh
@@ -2,6 +2,6 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GH_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/zen-browser-auto/www-temp/tags/protection \
+  https://api.github.com/repos/zen-browser-auto/www-temp/tags/ \
   > resp.json
   cat resp.json
