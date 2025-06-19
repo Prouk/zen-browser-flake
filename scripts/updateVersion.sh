2418ccfc9ba726f981ffde1f4596e9667452a935
Prouk <valentin.tahon2@gmail.com>
2025-06-19 22:00:01 +0200

    get only last tag

--- scripts/updateVersion.sh
+++ scripts/updateVersion.sh
@@ -3,5 +3,5 @@ curl -L \
   -H "Authorization: Bearer $GH_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/zen-browser-auto/www-temp/tags \
-  > resp.json
+  | jq -r '.[0].name' > resp.json
   cat resp.json
