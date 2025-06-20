52a0c7878113f275c258042a64eb7ad21f0c9840
Prouk <valentin.tahon2@gmail.com>
2025-06-20 21:22:37 +0200

    sha from tar directly

--- scripts/updateVersion.sh
+++ scripts/updateVersion.sh
@@ -13,7 +13,7 @@ sed -i -e '11a,' version.json
 curl -OL $(jq -r '.beta.tarball_url' version.json) > zen-beta.tar.gz
 curl -OL $(jq -r '.twilight.tarball_url' version.json) > zen-twilight.tar.gz
 
-sudo apt install coreutils
+sudo apt install hashalot
 
 BTSHA= $(shasum -a 256 zen-beta.tar.gz | awk '{print $1}')
 TWSHA= $(shasum -a 256 zen-beta.tar.gz | awk '{print $1}')
