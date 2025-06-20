307a90e9027d070c6327d28ef601fb702718c7b7
Prouk <valentin.tahon2@gmail.com>
2025-06-20 21:12:21 +0200

    sha from tar directly

--- scripts/updateVersion.sh
+++ scripts/updateVersion.sh
@@ -13,6 +13,8 @@ sed -i -e '11a,' version.json
 curl -OL $(jq -r '.beta.tarball_url' version.json) > zen-beta.tar.gz
 curl -OL $(jq -r '.twilight.tarball_url' version.json) > zen-twilight.tar.gz
 
+apt install shasum
+
 BTSHA= $(shasum -a 256 zen-beta.tar.gz | awk '{print $1}')
 TWSHA= $(shasum -a 256 zen-beta.tar.gz | awk '{print $1}')
 
