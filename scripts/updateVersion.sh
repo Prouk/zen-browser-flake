66f913594689a5e9656d891edbffb5b6000cdb87
Prouk <valentin.tahon2@gmail.com>
2025-06-22 17:56:27 +0200

    piping tar get into sha for less writing

--- scripts/updateVersion.sh
+++ scripts/updateVersion.sh
@@ -8,25 +8,8 @@ curl -L \
 sed -i '12,13d' version.json
 sed -i -e '11a,' version.json
 
-$(wget "$(jq -r '.beta.tarball_url' version.json)")
-$(wget "$(jq -r '.twilight.tarball_url' version.json)")
-
-mkdir beta_dir
-mkdir twilight_dir
-
-tar -xzf $(jq -r '.beta.name' version.json) -C beta_dir
-tar -xzf twilight -C twilight_dir
-
-# BTNAME=$(jq -r '.beta.name' version.json)
-# TWNAME=$(jq -r '.twilight.name' version.json)
-
-# BTSHA=$(sudo sha256sum "$BTNAME" | sudo awk '{print $1}')
-# TWSHA=$(sudo sha256sum "$TWNAME" | sudo awk '{print $1}')
-
-BTSHA=$(find twilight_dir -type f -print0 | sort -z | xargs -0 sha256sum | sha256sum)
-TWSHA=$(find beta_dir -type f -print0 | sort -z | xargs -0 sha256sum | sha256sum)
-
-echo $BTSHA
+BTSHA=$(wget "$(jq -r '.beta.tarball_url' version.json)" | sha256sum)
+TWSHA=$(wget "$(jq -r '.twilight.tarball_url' version.json)" | sha256sum)
 
 jq '.beta += {tarball_sha: "'"${BTSHA::-3}"'"} | .twilight += {tarball_sha: "'"${TWSHA::-3}"'"}' version.json > version.tmp && mv version.tmp version.json
 
--- zen-browser-twilight.nix
+++ zen-browser-twilight.nix
@@ -3,18 +3,9 @@ pkgs.stdenv.mkDerivation {
   pname = "zen-browser";
   version = version.twilight.name;
 
-  
-  let
-    sha="
-      nix-prefetch-url ${version.twilight.tarball_url}
-    ";
-  in
-
-  src = pkgs.fetchFromGitHub {
-    owner = "zen-browser";
-    repo = "desktop";
-    rev = version.twilight.commit.sha;
-    sha256 = sha;
+  src = builtins.fetchurl {
+    url = version.twilight.tarball_url;
+    sha256 = version.twilight.tarball_sha;
   };
 
   installPhase = ''
