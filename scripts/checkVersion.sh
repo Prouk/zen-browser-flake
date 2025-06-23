6b6f73b0e3d39057f0fe91f13dd6ff44f7b2c7ad
Prouk <valentin.tahon2@gmail.com>
2025-06-23 14:49:11 +0200

    correction to ci

--- scripts/checkVersion.sh
+++ scripts/checkVersion.sh
@@ -11,7 +11,7 @@ func GetLatestRelease {
     https://api.github.com/repos/zen-browser/desktop/releases/latest \
     | jq -r '.tag_name')`
 
-  $VERSION+=("$BETAVER")
+  $VERSION+="$BETAVER"
 }
 
 func GetReleaseByTag {
