19149d368932ad1c57375e71c16e6c838b7ea8f0
Prouk <valentin.tahon2@gmail.com>
2025-06-23 15:00:23 +0200

    tying to fix things again

--- scripts/checkVersion.sh
+++ scripts/checkVersion.sh
@@ -4,14 +4,14 @@ VERSION=(twilight)
 
 GetLatestRelease() {
   echo "Getting latest release tag name"
-  BETAVER=`curl -L \
+  BETAVER=$(curl -L \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $GH_TOKEN" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/repos/zen-browser/desktop/releases/latest \
-    | jq -r '.tag_name')`
+    | jq -r '.tag_name')
 
-  $VERSION+=("$BETAVER")
+  $VERSION+=($BETAVER)
 }
 
 GetReleaseByTag() {
