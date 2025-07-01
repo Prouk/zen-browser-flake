d04e79ce140e671006064c3747f9bc2ebc20d0e4
Prouk <valentin.tahon2@gmail.com>
2025-07-02 00:16:27 +0200

    going with envbubble

--- zen-twilight.nix
+++ zen-twilight.nix
@@ -1,42 +1,55 @@
 {
-  version,
-  pkgs ? import <nixpkgs> {}
+  pkgs ? import <nixpkgs> { },
+  version, libGL,
 }:
+
 let
-  libs = with pkgs; [
-  ];
+  zenPkg = pkgs.stdenv.mkDerivation {
+    pname = "zen-browser";
+    version = version.twilight.tag_name;
+
+    src = pkgs.fetchurl {
+      url = version.twilight.browser_download_url;
+      sha256 = version.twilight.digest;
+    };
+
+    installPhase = ''
+      mkdir -p $out
+      # Copy all extracted content into $out/zen
+      cp -r . $out
+    '';
+  };
 in
-pkgs.stdenv.mkDerivation {
-  pname = "zen";
-  version = version.twilight.tag_name;
 
-  src = pkgs.fetchurl {
-    url = version.twilight.browser_download_url;
-    sha256 = version.twilight.digest;
-  };
+pkgs.buildFHSEnvBubblewrap {
+  name = "zen-twilight-fhs";
+  targetPkgs =
+    pkgs:
+    (with pkgs; [
+      gtk3
+      glib
+      gdk-pixbuf
+      pango
+      atk
+      alsa-lib
+      pciutils
+      libGL
+      cairo
+      # Xorg dependencies
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libX11
+      xorg.libXi
+      xorg.libXdamage
+      xorg.libXfixes
+      xorg.libxcb
+      xorg.libXext
+      xorg.libXcomposite
+    ]);
+  profile = "export LD_LIBRARY_PATH=/run/opengl-driver/lib\n";
 
-  phases = [
-    "unpackPhase"
-    "installPhase"
-    "fixupPhase"
-  ];
-
-  buildInputs = with pkgs; [
-    makeWrapper
-    patchelf
-  ];
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp -r ./ $out/bin
-
-    isntall -D 
-  '';
-
-  meta = with pkgs.lib; {
-    description = "Zen Twilight";
-    homepage = "https://zen-browser.app/download/?twilight";
-    maintainers = with maintainers; [ Prouk ];
-    paltforms = platforms.linux;
-  };
+  # Find the right path to the Zen binary inside the extracted directory.
+  # If the tarball contains a subdirectory, adjust the path accordingly.
+  # For example, if it unpacks to zen-x.y.z/zen, use $zenPkg/zen-x.y.z/zen
+  runScript = "${zenPkg}/zen";
 }
